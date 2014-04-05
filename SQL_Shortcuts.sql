faff96f013c119284be283d14207c71fa8aea1e4
astarrywonder <saltyvanilla@hotmail.com>
2014-04-05 11:49:23 -0400

    Added Grant memo
    Because I'm lazy?

---
+++ SQL_Shortcuts.sql
@@ -0,0 +1,11 @@
+------------------------------
+- Lazy Code Shortcuts
+------------------------------
+
+GRANT ALL ON TABLE table_name TO { aevans | anderson}
+
+GRANT ALL ON ALL TABLES IN SCHEMA public TO { aevans | anderson}
+
+GRANT ALL ON DATABASE kuroko TO { aevans | anderson}
+
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO { aevans | anderson}
